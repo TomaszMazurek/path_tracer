ed292b6944afa7807d6f22dac300b3823ce92167
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-27 21:19:56 +0100

    ray to the scene

--- main.cpp
+++ main.cpp
@@ -19,7 +19,14 @@ int main() {
 
     //Camera
     auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio
+    auto viewport_width = aspect_ratio;
+    auto focal_length = 1.0; //odleglosc obserwatora od viewportu
+
+    auto origin = point3(0,0,0); // środek viewportu
+    auto horizontal = vec3(viewport_width, 0, 0); //szerokość viewportu
+    auto vertical = vec3(0, viewport_height, 0); //wysokość viewportu
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length); // początek układu współrzędnych viewportu
+
 
     //Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -27,7 +34,10 @@ int main() {
     for (int j = image_height -1; j >=0 ; --j){
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width ; ++i){
-            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            auto u = double(i) / (image_width-1); //współrzędna horyzontalna u
+            auto v = double(j) / (image_height-1); //współrzędna wertykalna v
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin); // ustawienie kierunku promienia względem (piksela) viewportu  
+            color pixel_color = ray_color(r); //wyznaczenie koloru w punkcie przecięcia promienia z (pikselem) vieportem
             write_color(std::cout, pixel_color);
         }
     }
