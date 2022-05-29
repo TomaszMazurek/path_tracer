e5684da25617b56ebd6b86fbf91a26ce0e6c9779
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-29 21:29:18 +0100

    camera vfov

--- main.cpp
+++ main.cpp
@@ -42,13 +42,19 @@ int main() {
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
     //auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    //auto material_left   = make_shared<dielectric>(1.5);
+    //auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.3,    0.0, -1.3),   -0.5, material_left));
-    world.add(make_shared<sphere>(point3( 1.3,    0.0, -1.3),   0.5, material_right));
+    //world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    //world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    //world.add(make_shared<sphere>(point3( R,    0.0, -1.3), R, material_left));
+    //world.add(make_shared<sphere>(point3( -R,    0.0, -1.3), R, material_right));
+
+    auto material_left  = make_shared<lambertian>(color(0,0,1));
+    auto material_right = make_shared<lambertian>(color(1,0,0));
+
+    world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
+    world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
 
     //camera
     camera cam(90.0, aspect_ratio);
