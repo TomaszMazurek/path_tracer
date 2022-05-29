c8acffaea39ffee52638ce9f99639a8c1fa4a80f
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-29 20:52:02 +0100

    fuzz added to reflective material

--- material.h
+++ material.h
@@ -34,19 +34,20 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
        bool scatter(
             const ray& r_in, const ray_hit_point& hit, color& attenuation, ray& scattered
         ) const {
             vec3 reflected = reflect(unit_vector(r_in.direction()), hit.normal);
-            scattered = ray(hit.p, reflected);
+            scattered = ray(hit.p, reflected + fuzz*random_unit_hit_on_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), hit.normal) > 0);
         }
 
     public:
         color albedo;
+        double fuzz;
 };
 
 #endif
