2cf8537e925aaa1b374dc6bce71977451191a0ff
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-30 21:00:43 +0100

    importance sampling 1

--- math_samples/integrate_x_sq.cpp
+++ math_samples/integrate_x_sq.cpp
@@ -6,14 +6,14 @@
 #include <stdlib.h>
 
 inline double pdf(double x) {
-    return 0.5*x;
+    return 0.5;
 }
 
 int main() {
     int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        double x = sqrt(rng(0,4));
+        double x = rng(0,2);
         sum += x*x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
