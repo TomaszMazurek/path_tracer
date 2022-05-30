1d52d4da6039d006ddac76c67cdd513f13cb0067
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-30 16:30:14 +0100

    image texture and stb_image

--- main.cpp
+++ main.cpp
@@ -50,6 +50,14 @@ object3d_list two_perlin_spheres() {
     return objects;
 }
 
+object3d_list earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    return object3d_list(globe);
+}
+
 object3d_list random_scene() {
     object3d_list world;
 
@@ -140,13 +148,20 @@ int main() {
             vfov = 20.0;
             break;
         
-        default:
         case 3:
             world = two_perlin_spheres();
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
             break;    
+
+        default:
+        case 4:
+            world = earth();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
     }
 
     // Camera
