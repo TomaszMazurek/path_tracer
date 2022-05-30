a01e6939d5c2d8434ab5b59b5291fc129d956ee1
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-30 15:22:00 +0100

    perlin noise turbulence with phase

--- texture.h
+++ texture.h
@@ -54,7 +54,7 @@ class noise_texture : public texture {
         noise_texture(double sc) : scale(sc) {}
 
         virtual color value(double u, double v, const point3& p) const override {
-            return color(1,1,1) * noise.turb(scale * p);
+            return color(1,1,1) * 0.5 * (1 + sin(scale*p.z() + 10*noise.turb(p)));
         }
 
     public:
