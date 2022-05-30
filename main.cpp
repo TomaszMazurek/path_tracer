07242e3ee85f9403ee795f1d14d53175aab44145
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-30 14:56:15 +0100

    checker solid texture on sphere

--- main.cpp
+++ main.cpp
@@ -28,6 +28,17 @@ color ray_color(const ray& r, const object3d& world, int depth) {
     return  (1.0-t) * color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
+object3d_list two_spheres() {
+    object3d_list objects;
+
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+    objects.add(make_shared<sphere>(point3(0,-10, 0), 10, make_shared<lambertian>(checker)));
+    objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    return objects;
+}
+
 object3d_list random_scene() {
     object3d_list world;
 
@@ -91,19 +102,52 @@ int main() {
     int samples_per_pixel = 100;
 
     //const int image_height = static_cast<int>(image_width/ aspect_ratio);   
-    int image_height = static_cast<int>(image_width / aspect_ratio);
+    //int image_height = static_cast<int>(image_width / aspect_ratio);
 
     const int max_depth = 50;
 
-    //World
-    //auto R = cos(pi/4);
-    auto world = random_scene();
-    
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
+    object3d_list world;
+
+    point3 lookfrom;
+    point3 lookat;
+    auto vfov = 40.0;
+    auto aperture = 0.0;
+
+    switch (0) {
+        case 1:
+            world = random_scene();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            aperture = 0.1;
+            break;
+
+        default:
+        case 2:
+            world = two_spheres();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
+    }
+
+    // Camera
+
     vec3 vup(0,1,0);
     auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+
+    //World
+    //auto R = cos(pi/4);
+    //auto world = random_scene();
+    
+    //point3 lookfrom(13,2,3);
+    //point3 lookat(0,0,0);
+    //vec3 vup(0,1,0);
+    //auto dist_to_focus = 10.0;
+    //auto aperture = 0.1;
 
     //auto material_ground = make_shared<lambertian>(color(0.2, 0.2, 0.2));
     //auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.5));
@@ -139,7 +183,7 @@ int main() {
     //camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
 
     //camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+    //camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     //Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
