8dc253f2ea9c4c7d060145cd61dae927420c11a0
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-30 21:04:00 +0100

    MC Integration on the Sphere of Directions

--- math_samples/integrate_x_sq.cpp
+++ math_samples/integrate_x_sq.cpp
@@ -5,17 +5,18 @@
 #include <math.h>
 #include <stdlib.h>
 
-inline double pdf(double x) {
-    return 3*x*x/8;
+inline double pdf(const vec3& p) {
+    return 1 / (4*pi);
 }
 
 int main() {
-    int N = 1;
+    int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = pow(rng(0,8), 1./3.);
-        sum += x*x / pdf(x);
+        vec3 d = random_unit_hit_on_sphere();
+        auto cosine_squared = d.z()*d.z();
+        sum += cosine_squared / pdf(d);
     }
     std::cout << std::fixed << std::setprecision(12);
-    std::cout << "I = " << 2 * sum/N << '\n';
+    std::cout << "I = " << sum/N << '\n';
 }
