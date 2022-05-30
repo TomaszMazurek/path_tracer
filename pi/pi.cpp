b873395c88f97c6b445a990aaed3d70e60882825
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-30 20:25:43 +0100

    Regular    Estimate of Pi = 3.141415560000 Stratified Estimate of Pi = 3.141592120000

--- pi/pi.cpp
+++ pi/pi.cpp
@@ -7,20 +7,26 @@
 
 int main() {
     int inside_circle = 0;
-    int runs = 0;
-    std::cout << std::fixed << std::setprecision(12);
-    const clock_t begin_time = clock();
-    while (true) {
-        runs++;
-        auto x = rng(-1,1);
-        auto y = rng(-1,1);
-        if (x*x + y*y < 1)
-            inside_circle++;
-
-        if (runs % 100000 == 0)
-            std::cout << "Estimate of Pi = "
-                      << 4*double(inside_circle) / runs
-                      << " - " << float( clock () - begin_time ) /  CLOCKS_PER_SEC
-                      << '\n';
+    int inside_circle_stratified = 0;
+    int sqrt_N = 10000;
+    for (int i = 0; i < sqrt_N; i++) {
+        for (int j = 0; j < sqrt_N; j++) {
+            auto x = rng(-1,1);
+            auto y = rng(-1,1);
+            if (x*x + y*y < 1)
+                inside_circle++;
+            x = 2*((i + rng()) / sqrt_N) - 1;
+            y = 2*((j + rng()) / sqrt_N) - 1;
+            if (x*x + y*y < 1)
+                inside_circle_stratified++;
+        }
     }
+
+    auto N = static_cast<double>(sqrt_N) * sqrt_N;
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout
+        << "Regular    Estimate of Pi = "
+        << 4*double(inside_circle) / (sqrt_N*sqrt_N) << '\n'
+        << "Stratified Estimate of Pi = "
+        << 4*double(inside_circle_stratified) / (sqrt_N*sqrt_N) << '\n';
 }
