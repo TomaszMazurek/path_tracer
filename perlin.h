0eba344024df72ee4aa6ad8e2171a3abecd9e62e
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-30 15:16:24 +0100

    perlin noise scaling using random rectors on the lattice points

--- perlin.h
+++ perlin.h
@@ -6,9 +6,9 @@
 class perlin {
     public:
         perlin() {
-            ranfloat = new double[point_count];
+            ranvec = new vec3[point_count];
             for (int i = 0; i < point_count; ++i) {
-                ranfloat[i] = rng();
+                ranvec[i] = unit_vector(vec3::random(-1,1));
             }
 
             perm_x = perlin_generate_perm();
@@ -17,7 +17,7 @@ class perlin {
         }
 
         ~perlin() {
-            delete[] ranfloat;
+            delete[] ranvec;
             delete[] perm_x;
             delete[] perm_y;
             delete[] perm_z;
@@ -35,23 +35,23 @@ class perlin {
             auto i = static_cast<int>(floor(p.x()));
             auto j = static_cast<int>(floor(p.y()));
             auto k = static_cast<int>(floor(p.z()));
-            double c[2][2][2];
+            vec3 c[2][2][2];
 
             for (int di=0; di < 2; di++)
                 for (int dj=0; dj < 2; dj++)
                     for (int dk=0; dk < 2; dk++)
-                        c[di][dj][dk] = ranfloat[
+                        c[di][dj][dk] = ranvec[
                             perm_x[(i+di) & 255] ^
                             perm_y[(j+dj) & 255] ^
                             perm_z[(k+dk) & 255]
                         ];
 
-            return trilinear_interp(c, u, v, w);
+            return perlin_interp(c, u, v, w);
         }
 
     private:
         static const int point_count = 256;
-        double* ranfloat;
+        vec3* ranvec;
         int* perm_x;
         int* perm_y;
         int* perm_z;
@@ -84,6 +84,25 @@ class perlin {
                         accum += (i*u + (1-i)*(1-u))*
                                 (j*v + (1-j)*(1-v))*
                                 (k*w + (1-k)*(1-w))*c[i][j][k];
+
+            return accum;
+        }
+
+        static double perlin_interp(vec3 c[2][2][2], double u, double v, double w) {
+            auto uu = u*u*(3-2*u);
+            auto vv = v*v*(3-2*v);
+            auto ww = w*w*(3-2*w);
+            auto accum = 0.0;
+
+            for (int i=0; i < 2; i++)
+                for (int j=0; j < 2; j++)
+                    for (int k=0; k < 2; k++) {
+                        vec3 weight_v(u-i, v-j, w-k);
+                        accum += (i*uu + (1-i)*(1-uu))
+                               * (j*vv + (1-j)*(1-vv))
+                               * (k*ww + (1-k)*(1-ww))
+                               * dot(c[i][j][k], weight_v);
+                    }
 
             return accum;
         }
