9452485dacc6d7c95a665392f3c69e4ca775d19e
Tomasz Mazurek <tomasz.a.mazurek@gmail.com>
2022-05-30 19:56:55 +0100

    BVH applied to final scene

--- object3d/bvh.h
+++ object3d/bvh.h
@@ -3,7 +3,7 @@
 
 #include <algorithm>
 
-#include "utils.h"
+#include "../utils.h"
 #include "object3d.h"
 #include "object3d_list.h"
 
@@ -11,7 +11,7 @@
 class bvh_node : public object3d {
     public:
         bvh_node();
-        bvh_node(const object3d_list& list, double time0, double time1): bvh_node(list.objects, 0, list.objects.size(), time0, time1){}
+        bvh_node(const object3d_list& list, double time0, double time1) : bvh_node(list.objects, 0, list.objects.size(), time0, time1){}
         bvh_node(const std::vector<shared_ptr<object3d>>& src_objects, size_t start, size_t end, double time0, double time1);
 
         virtual bool hit(const ray& r, double t_min, double t_max, ray_hit_point& hit) const override;
@@ -22,44 +22,6 @@ class bvh_node : public object3d {
         shared_ptr<object3d> right;
         aabb box;
 };
-
-bvh_node::bvh_node(std::vector<shared_ptr<object3d>>& src_objects, size_t start, size_t end, double time0, double time1) {
-    auto objects = src_objects; // Create a modifiable array of the source scene objects
-
-    int axis = random_int(0,2);
-    auto comparator = (axis == 0) ? box_x_compare
-                    : (axis == 1) ? box_y_compare
-                                  : box_z_compare;
-
-    size_t object_span = end - start;
-
-    if (object_span == 1) {
-        left = right = objects[start];
-    } else if (object_span == 2) {
-        if (comparator(objects[start], objects[start+1])) {
-            left = objects[start];
-            right = objects[start+1];
-        } else {
-            left = objects[start+1];
-            right = objects[start];
-        }
-    } else {
-        std::sort(objects.begin() + start, objects.begin() + end, comparator);
-
-        auto mid = start + object_span/2;
-        left = make_shared<bvh_node>(objects, start, mid, time0, time1);
-        right = make_shared<bvh_node>(objects, mid, end, time0, time1);
-    }
-
-    aabb box_left, box_right;
-
-    if (  !left->bounding_box (time0, time1, box_left)
-       || !right->bounding_box(time0, time1, box_right)
-    )
-        std::cerr << "No bounding box in bvh_node constructor.\n";
-
-    box = surrounding_box(box_left, box_right);
-}
 
 bool bvh_node::hit(const ray& r, double t_min, double t_max, ray_hit_point& hit) const {
     if (!box.hit(r, t_min, t_max))
@@ -99,4 +61,41 @@ bool box_z_compare (const shared_ptr<object3d> a, const shared_ptr<object3d> b) 
     return box_compare(a, b, 2);
 }
 
+bvh_node::bvh_node(const std::vector<shared_ptr<object3d>>& src_objects, size_t start, size_t end, double time0, double time1) {
+    auto objects = src_objects; // Create a modifiable array of the source scene objects
+
+    int axis = random_int(0,2);
+    auto comparator = (axis == 0) ? box_x_compare
+                    : (axis == 1) ? box_y_compare
+                                  : box_z_compare;
+
+    size_t object_span = end - start;
+
+    if (object_span == 1) {
+        left = right = objects[start];
+    } else if (object_span == 2) {
+        if (comparator(objects[start], objects[start+1])) {
+            left = objects[start];
+            right = objects[start+1];
+        } else {
+            left = objects[start+1];
+            right = objects[start];
+        }
+    } else {
+        std::sort(objects.begin() + start, objects.begin() + end, comparator);
+
+        auto mid = start + object_span/2;
+        left = make_shared<bvh_node>(objects, start, mid, time0, time1);
+        right = make_shared<bvh_node>(objects, mid, end, time0, time1);
+    }
+
+    aabb box_left, box_right;
+
+    if (  !left->bounding_box (time0, time1, box_left)
+       || !right->bounding_box(time0, time1, box_right)
+    )
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+
+    box = surrounding_box(box_left, box_right);
+}
 #endif
